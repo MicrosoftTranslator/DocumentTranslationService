3970e7f1d11657a9a114222d30ca5cd2f75f011c
Chris Wendt <chris.wendt@outlook.com>
2022-05-12 17:19:56 -0700

    Fixing issue #88. Allowing multiple flight parameters.

--- FlightPolicy.cs
+++ FlightPolicy.cs
@@ -5,21 +5,27 @@ namespace DocumentTranslationService
 {
     internal class FlightPolicy : HttpPipelineSynchronousPolicy
     {
-        private readonly string flightString;
+        private readonly string[] flightStrings;
         /// <summary>
         /// Sets the string to be used in a flight
         /// </summary>
         /// <param name="flightString">the string to be used in a flight</param>
         public FlightPolicy(string flightString)
         {
-            this.flightString = flightString;
+            char[] splitchars = { ',', ';', ' ' };
+            this.flightStrings = flightString.Split(splitchars);
         }
 
         public override void OnSendingRequest(HttpMessage message)
             {
                 if (message.Request.Method.Method == "POST")
                 {
-                    message.Request.Uri.AppendQuery("flight", flightString);
+                foreach (string s in flightStrings)
+                {
+                    string st = s.Trim();
+                    if (!string.IsNullOrEmpty(st))
+                        message.Request.Uri.AppendQuery("flight", st);
+                }
                 }
             }
     }
